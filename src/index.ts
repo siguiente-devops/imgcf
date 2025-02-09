702352dfdb84a1c667dc69aa2ad061012c08e361
siguiente-devops <github@siguiente.com>
2025-02-09 03:00:39 -0500

    Kittens

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt: `cyberpunk ${Math.random() * 10 < 5 ? "cat" : "kitten"}`
     };
 
     const response = await env.AI.run(
